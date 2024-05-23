4eef3c9850274d82eaaeb106578aa26602106275
Kévin Bibollet <bibollet.kevin@gmail.com>
2024-05-24 00:11:43 +0200

    not hiding HUD anymore on elimination

--- src/Core/PlayerHUDManager.cs
+++ src/Core/PlayerHUDManager.cs
@@ -22,17 +22,6 @@ public class PlayerHUDManager__Update {
 
         __instance.playerPosSuffix.text = $"<space=0.1em>/<space=0.1em>{amountOfPlayersAlive}";
 
-        // Hide useless HUD elements when current player is eliminated.
-        bool isEliminated = (bool)player.Get("isEliminated", false);
-
-        if (isEliminated) {
-            __instance.lapCountParent.gameObject.SetActive(false);
-            __instance.timerParent.gameObject.SetActive(false);
-            __instance.minimapParent.gameObject.SetActive(false);
-            __instance.heartsPresentParent.gameObject.SetActive(false);
-            __instance.playersScoreParent.gameObject.SetActive(false);
-        }
-
         // Display correct amount of required laps.
         __instance.iLastVisibleLapCount = Game.Get().GetAmountOfLaps();
     }
